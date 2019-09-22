c89310aec3e76cf70614641ac798882ad1228cc7
Nahian Alam <nahian18@outlook.com>
2019-09-22 14:16:34 -0500

    Updates

---
+++ precache-manifest.d5afc06de242b6e7f8134aad1441dbab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa57ae4b86be57f5727d64e4f85cfbac",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "31d220664309f74baa0d",
+    "url": "/portfolio/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "2cc8a9b70acebea7a750",
+    "url": "/portfolio/static/js/2.fadd7498.chunk.js"
+  },
+  {
+    "revision": "31d220664309f74baa0d",
+    "url": "/portfolio/static/js/main.88c89d01.chunk.js"
+  },
+  {
+    "revision": "c98fad0fa5c777d28f1b",
+    "url": "/portfolio/static/js/runtime-main.36963274.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/portfolio/static/media/logo.25bf045c.svg"
+  }
+]);
