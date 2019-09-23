a06727ee20db2131cb18cd01f8a944bac9f85345
Nahian Alam <nahian18@outlook.com>
2019-09-22 21:38:08 -0500

    Redesigned the contact page

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -3,40 +3,40 @@ import gitlogo from "./github.png";
 import linkedinlogo from "./linkedin.png";
 import instagramlogo from "./instagram.png";
 import emaillogo from "./email.png";
+import handshake from "./handshake.png";
+import CollectionsPage from "./form";
 
 class Pane3 extends Component {
   state = {};
   render() {
     return (
       <React.Fragment>
-        <div className="App-header">
-          <h3 className="blueh">Check my Github</h3>
+        <div className="App">
+          <img src={handshake} className="logoC" alt="logoC"></img>
+          <br></br>
           <a href="https://github.com/nahianalam18">
-            <img src={gitlogo} className="logoC" alt="logo"></img>
+            <img src={gitlogo} className="ssCcont" alt="logo"></img>
           </a>
-        </div>
-        <div className="App-header">
-          <h3 className="blueh">Let's connect</h3>
           <a href="https://www.linkedin.com/in/nahian-alam-a2b521155/">
-            <img src={linkedinlogo} className="logoC" alt="logo"></img>
+            <img src={linkedinlogo} className="ssCcont" alt="logo"></img>
           </a>
-        </div>
-        <div className="App-header">
-          <h3 className="blueh">Follow me on Instagram</h3>
           <a href="https://www.instagram.com/nahian_alam/">
-            <img src={instagramlogo} className="logoC" alt="logo"></img>
+            <img src={instagramlogo} className="ssCcont" alt="logo"></img>
           </a>
-        </div>
-        <div className="App-header">
-          <h3 className="ph">
-            Send me an email -{" "}
-            <a className="App-link" href="mailto:alam.nahian18@gmail.com">
-              alam.nahian18@gmail.com
-            </a>
-          </h3>
+          <a className="App-link" href="mailto:alam.nahian18@gmail.com"></a>
           <a href="mailto:alam.nahian18@gmail.com">
-            <img src={emaillogo} className="logoC" alt="logo"></img>
+            <img src={emaillogo} className="ssCcont" alt="logo"></img>
           </a>
+          <br></br>
+          <footer>
+            <p className="blueh">
+              Contact information:{" "}
+              <a className="App-link" href="mailto:alam.nahian18@gmail.com">
+                alam.nahian18@gmail.com
+              </a>
+            </p>
+            <p className="blueh">© Nahian Alam</p>
+          </footer>
         </div>
       </React.Fragment>
     );
---
+++ src/components/form.jsx
@@ -0,0 +1,106 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "antd/dist/antd.css";
+import { Button, Modal, Form, Input, Radio } from "antd";
+
+const CollectionCreateForm = Form.create({ name: "form_in_modal" })(
+  // eslint-disable-next-line
+  class Entry extends Component {
+    render() {
+      const { visible, onCancel, onCreate, form } = this.props;
+      const { getFieldDecorator } = form;
+      return (
+        <Modal
+          visible={visible}
+          title="Create a new collection"
+          okText="Create"
+          onCancel={onCancel}
+          onOk={onCreate}
+        >
+          <Form
+            action="alam.nahian18@gmail.com"
+            method="post"
+            enctype="text/plain"
+            layout="vertical"
+          >
+            <Form.Item label="Your Name">
+              {getFieldDecorator("title", {
+                rules: [
+                  {
+                    required: true,
+                    message: "Please enter your name"
+                  }
+                ]
+              })(<Input />)}
+            </Form.Item>
+            <Form.Item label="Your email">
+              {getFieldDecorator("senderemail")(<Input type="textarea" />)}
+            </Form.Item>
+            <Form.Item label="Description">
+              {getFieldDecorator("description")(<Input type="textarea" />)}
+            </Form.Item>
+            <Form.Item className="collection-create-form_last-form-item">
+              {getFieldDecorator("modifier", {
+                initialValue: "public"
+              })(
+                <Radio.Group>
+                  <Radio value="public">Public</Radio>
+                  <Radio value="private">Private</Radio>
+                </Radio.Group>
+              )}
+            </Form.Item>
+          </Form>
+        </Modal>
+      );
+    }
+  }
+);
+
+class CollectionsPage extends Component {
+  state = {
+    visible: false
+  };
+
+  showModal = () => {
+    this.setState({ visible: true });
+  };
+
+  handleCancel = () => {
+    this.setState({ visible: false });
+  };
+
+  handleCreate = () => {
+    const { form } = this.formRef.props;
+    form.validateFields((err, values) => {
+      if (err) {
+        return;
+      }
+
+      console.log("Received values of form: ", values);
+      form.resetFields();
+      this.setState({ visible: false });
+    });
+  };
+
+  saveFormRef = formRef => {
+    this.formRef = formRef;
+  };
+
+  render() {
+    return (
+      <div>
+        <Button type="primary" onClick={this.showModal}>
+          Click to Email me
+        </Button>
+        <CollectionCreateForm
+          wrappedComponentRef={this.saveFormRef}
+          visible={this.state.visible}
+          onCancel={this.handleCancel}
+          onCreate={this.handleCreate}
+        />
+      </div>
+    );
+  }
+}
+
+export default CollectionsPage;
